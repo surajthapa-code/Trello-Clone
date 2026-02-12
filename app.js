f0a19ec5eff826974aa0142040b3d6c00086f2ed
Suraj Thapa <surajthapa7787@gmail.com>
2026-02-12 09:00:44 +0530

    now able to create new cards inside the list, after the card is created input field will disable

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const MainContainer = document.getElementById("MainContainer");
 const ListHeading = document.getElementsByClassName("ListHeading");
 const NewCardBtn = document.querySelector(".AddNewCard");
 const CardList = document.querySelector(".CardList");
+let TempInput;
 
-
-
+function InputDisable (Event){
+        if(Event.key == "Enter"){
+            TempInput.disabled = true;
+            TempInput.style.display = "none"
+        }
+    }
+    
 function tempInp4CardL (){
-    let TempInput = document.createElement("input");
+    TempInput = document.createElement("input");
     CardList.appendChild(TempInput);
+    TempInput.id = "temp"
     TempInput.focus();
+
 
     TempInput.addEventListener("keydown", function (Event){
         if(Event.key === "Enter"){
@@ -18,8 +26,9 @@ function tempInp4CardL (){
                 let li = document.createElement("li");
                 li.textContent = CardData;
                 CardList.appendChild(li);
-                
             }
+                   InputDisable(Event);
+
         }
                 
     })
@@ -30,3 +39,5 @@ function tempInp4CardL (){
 NewCardBtn.addEventListener("click", print =()=>{
         tempInp4CardL();
 })
+
+
